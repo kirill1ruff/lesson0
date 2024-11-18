688283546158fac3102b16ea539abb21ea88f2f5
kirill1ruff <search3149@gmail.com>
2024-11-18 14:30:59 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+name = "Name: Kirill"
+print(name)
+name = "Age: 25"
+print (name)
+name = "New Age:"
+age = 25
+new_age = 5
+print (name, age+new_age)
+name = "Is Student:"
+is_student = True
+print (name, is_student)
