baa6407bf8adafa3dcf23e0781fe0f9d75b68127
kirill1ruff <search3149@gmail.com>
2024-11-18 15:16:42 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+well='Python,'
+total_tasks='12,'
+hours_spent='1.5,'
+average_execution_time='0.125 часа.'
+print ('Курс:',well,'всего задач:',total_tasks,'затрачено часов',hours_spent,'среднее время выполнения',average_execution_time)
