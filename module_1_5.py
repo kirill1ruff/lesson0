15822fdffa728b0799e17a59b1a7d4713ca2bc54
kirill1ruff <search3149@gmail.com>
2024-11-19 09:37:42 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,18 @@
+immutable_var = (1,2,3,'a','b')
+print(immutable_var)
+# Попытка изменить элементы кортежа immutable_var
+#immutable_var[0] = 3
+#Нельзя изменить значение элемента, потому что кортежи неизменяемы.
+mutable_list = ['a','b',6,7,8,True,False,'a'+'b']
+mutable_list[0]='Thank'
+mutable_list[1]='you'
+mutable_list[2]='for'
+mutable_list[3]='the'
+mutable_list[4]='opportunity'
+mutable_list[5]='to'
+mutable_list[6]='learn'
+mutable_list[7]='programming'
+print(mutable_list)
+
+
+
