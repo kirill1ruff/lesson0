94f0dad23267ba4869771d9eb4ed5e4ad48b8760
kirill1ruff <search3149@gmail.com>
2024-11-25 12:38:56 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -1,5 +1,5 @@
-well='Python,'
-total_tasks='12,'
-hours_spent='1.5,'
-average_execution_time='0.125 часа.'
-print ('Курс:',well,'всего задач:',total_tasks,'затрачено часов',hours_spent,'среднее время выполнения',average_execution_time)
+homework_done = 12  # Количество выполненных ДЗ
+hours_spent = 1.5   # Количество затраченных часов
+course_name = 'Python'  # Название курса
+time_per_assignment = hours_spent / homework_done
+print(f"Курс: {course_name}, всего задач: {homework_done}, затрачено часов: {hours_spent}, среднее время выполнения {time_per_assignment:.3f} часа.")
