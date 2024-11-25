c9b7a39c72b11440c49608a198000fd0c7f8edd7
kirill1ruff <search3149@gmail.com>
2024-11-25 12:39:48 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -1,5 +1,5 @@
-homework_done = 12  # Количество выполненных ДЗ
-hours_spent = 1.5   # Количество затраченных часов
-course_name = 'Python'  # Название курса
+homework_done = 12 
+hours_spent = 1.5  
+course_name = 'Python'  
 time_per_assignment = hours_spent / homework_done
 print(f"Курс: {course_name}, всего задач: {homework_done}, затрачено часов: {hours_spent}, среднее время выполнения {time_per_assignment:.3f} часа.")
